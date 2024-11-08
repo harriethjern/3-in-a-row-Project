e08f8bf9930346ee0b70c05b5a23e5c8bfc09db7
annsofjoh <annsofjoh@gmail.com>
2024-11-08 11:37:10 +0100

    update3

--- app/src/main/java/com/example/a3_in_a_row_project/MainActivity.kt
+++ app/src/main/java/com/example/a3_in_a_row_project/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             _3inarowprojectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android111111",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
